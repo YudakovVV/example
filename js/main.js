435377439608ebac3fe66ba8161a4f729df7a890
skiff2140 <skiff2140@gmail.com>
2020-03-15 21:37:00 +0400

    feat: section project and swiper slider

--- js/main.js
+++ js/main.js
@@ -45,7 +45,24 @@ btn.on('click', function(e) {
   $('html, body').animate({scrollTop:0}, '300');
 });
 
+//initialize swiper when document ready
+var mySwiper = new Swiper ('.swiper-container', {    
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+var next = $('.swiper-button-next');
+var prev = $('.swiper-button-prev');
+var bullets = $('.swiper-pagination');
 
+next.css('left', prev.width() + 10 + bullets.width() + 10)
+bullets.css('left', prev.width() + 10)
 
 
 })
