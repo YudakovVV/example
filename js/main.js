ceb3b082f07c043b41d59c70c4186f3812cfaa18
skiff2140 <skiff2140@gmail.com>
2020-03-15 20:12:06 +0400

    feat: section project

--- js/main.js
+++ js/main.js
@@ -29,5 +29,24 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+///////////////кнопка прокрутки///////////////
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
+
 })
 
