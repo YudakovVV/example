fd87ecd28b9ccb9aad24ed428ee8c008539fcf50
skiff2140 <skiff2140@gmail.com>
2020-03-19 22:19:38 +0400

    feat: add error checked and format form

--- js/main.js
+++ js/main.js
@@ -66,7 +66,38 @@ bullets.css('left', prev.width() + 10)
 
 new WOW().init();
 
+//Валидация формы
+$('.modal__form').validate({
+  errorClass: "invalid",
+  rules: {
+    // строчное правило
+    userName: {
+      required: true,
+      minlength: 2
+    },
+    userPhone: "required",
+    // правило-объект
+    userEmail: {
+      required: true,
+      email: true
+    }
+  }, //сообщения
+  messages: {
+    userName: {
+      required: "Имя обязательно",
+      minlength: "Имя не короче двух букв"
+    },
+    userPhone: "Телефон обязателен",
+    userEmail: {
+      required: "Обязательно укажите Email",
+      email: "Введите в формате name@domain.com"
+    }
+  }
+});
 
+//маска для номера телефона
+
+$('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7 (__) ___-__-__"});
 
 })
 
